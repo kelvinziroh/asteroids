daa6625fec716c1ac64a735ceca19c1f122a191b
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-06-30 19:46:33 +0300

    Create a basic game loop

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-# Import the pygame module
+# Import necessary modules
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    # Initialize pygame
+    pygame.init()
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Get a new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Create a basic game loop
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
