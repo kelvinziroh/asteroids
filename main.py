1da9a6a78790da6eda0b0f1eecc8417e33d0bd0a
Kelvin Ziro <kelvin.ziroh@gmail.com>
2025-07-01 14:36:37 +0300

    Limit the asteroids window to 60 fps

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ def main():
     # Get a new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    # Create a clock object
+    clock = pygame.time.Clock()
+    
+    # Initialize a variable to 0
+    dt = 0
+    
     # Create a basic game loop
     while True:
         screen.fill("black")
         pygame.display.flip()
 
+        # Limit the program's fps to 60 and record the delta 
+        # time in seconds
+        dt = clock.tick(60) / 1000
+        
         # Exit game loop once user closes the game window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
